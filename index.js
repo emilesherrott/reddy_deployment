4020e38c242e9e6f524fbabfed896eb777872264
“emilesherrott” <emilesherrott@gmail.com>
2023-10-31 15:00:17 +0000

    CORS middleware added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require("express")
+const cors = require("cors")
 const fs = require("fs")
 const app = express()
 
 const logger = require("./logger")
 
 app.use(express.json())
+app.use(cors())
 app.use(logger)
 
 // ! DATA IMPORT
